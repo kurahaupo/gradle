46bad16c7ecb44fdedf70a3da18942a5d9644f3a
Martin D Kealey <martin@kurahaupo.gen.nz>
2021-05-19 16:56:11 +1000

    Remove bash dependency from check-rev.sh
    Signed-off-by: Martin D Kealey <martin@kurahaupo.gen.nz>

--- subprojects/performance/docs/check-rev.sh
+++ subprojects/performance/docs/check-rev.sh
@@ -1,18 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 # script to be used for Gradle performance test bisecting
 # example usage:
 # git bisect start HEAD REL_2.14 --  # HEAD=bad REL_2.14=good
 # git bisect run check_rev.sh JavaConfigurationPerformanceTest lotDependencies
+
 TESTNAME=${1:-IdeIntegrationPerformanceTest}
 TESTPROJECT=${2:-multi}
+
+gbo="$HOME"/.gradle-bisect-override
+gbs="$HOME"/.gradle-bisect-override-script
+gbr="$HOME"/.gradle-bisect-results
+
 ./gradlew clean
-[ -d ~/.gradle-bisect-override ] && cp -Rdvp ~/.gradle-bisect-override/* .
-[ -x ~/.gradle-bisect-override-script ] && ~/.gradle-bisect-override-script $TESTNAME $TESTPROJECT
-./gradlew -S -PtimestampedVersion -x :performance:prepareSamples :performance:$TESTPROJECT :performance:cleanPerformanceTest :performance:performanceTest -D:performance:performanceTest.single=$TESTNAME
+
+[ -d "$gbo" ] && cp -Rdvp "$gbo"/* .
+[ -x "$gbs" ] && "$gbs" "$TESTNAME" "$TESTPROJECT"
+./gradlew -S -PtimestampedVersion \
+          -x :performance:prepareSamples \
+             :performance:"$TESTPROJECT" \
+             :performance:cleanPerformanceTest \
+             :performance:performanceTest \
+           -D:performance:performanceTest.single="$TESTNAME"
 result=$?
-hash=$(git rev-parse HEAD | colrm 9)
-datets=$(date +%Y-%m-%d-%H:%M:%S)
-[ -d ~/.gradle-bisect-results ] || mkdir ~/.gradle-bisect-results
-cp subprojects/performance/build/test-results/performanceTest/TEST-org.gradle.performance.$TESTNAME.xml ~/.gradle-bisect-results/result_${result}_${hash}_${datets}.xml
+datets=$( date +%F-%T )
+
+hash=$( git rev-parse --short HEAD )
+
+mkdir -p "$gbr"
+cp "subprojects/performance/build/test-results/performanceTest/TEST-org.gradle.performance.$TESTNAME.xml" \
+   "$gbr/result_${result}_${hash}_${datets}.xml"
+
 git reset --hard
 exit $result
