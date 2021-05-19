f482708586dd6790cba8877a33c9dfd0c6b2aeec
Martin D Kealey <martin@kurahaupo.gen.nz>
2021-05-19 16:55:38 +1000

    Let "read" builtin take care of echo

--- subprojects/docs/src/samples/credentials-handling/pass-credentials-to-external-tool-via-stdin/common/login.sh
+++ subprojects/docs/src/samples/credentials-handling/pass-credentials-to-external-tool-via-stdin/common/login.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo Enter username:
-read username
-echo Enter password:
-if [ -t 0 ] ; then # if connected to a terminal, do not echo the password input
-    stty -echo
-    read password
-    stty echo
-    echo
-else
-    read password
-fi
+# Bash all the way back to v2.05 has read with -p and -s
+# where both -s and -p are ignored if stdin isn't a tty.
+
+read -p 'Enter username: ' username || exit
+read -p 'Enter password: ' -s password || exit
+
+printf '\n'
 
 if [ "$username" = "secret-user" ] && [ "$password" = "secret-password" ] ; then
     echo "Welcome, $username!"
